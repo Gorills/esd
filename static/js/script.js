25c2d436d42d6f99c906cb2533b16947715de9fe
Gorills <gorivanickiy@gmail.com>
2021-07-09 19:39:16 +0700

    services fix

--- static/js/script.js
+++ static/js/script.js
@@ -242,3 +242,57 @@ $(".rewiew__open").click(function (e) {
   });
   $(this).toggleText('подробнее', 'скрыть');
 });
+$('#ser1').click(function () {
+  $('#item1').addClass('services__item--active');
+  $('#item2, #item3, #item4, #item5, #item6, #item7, #item8, #item9').removeClass('services__item--active');
+  $('#ser2, #ser3, #ser4, #ser5, #ser6, #ser7, #ser8, #ser9').removeClass('panel__item--active');
+  $(this).addClass('panel__item--active');
+});
+$('#ser2').click(function () {
+  $('#item2').addClass('services__item--active');
+  $('#item1, #item3, #item4, #item5, #item6, #item7, #item8, #item9').removeClass('services__item--active');
+  $('#ser1, #ser3, #ser4, #ser5, #ser6, #ser7, #ser8, #ser9').removeClass('panel__item--active');
+  $(this).addClass('panel__item--active');
+});
+$('#ser3').click(function () {
+  $('#item3').addClass('services__item--active');
+  $('#item1, #item2, #item4, #item5, #item6, #item7, #item8, #item9').removeClass('services__item--active');
+  $('#ser1, #ser2, #ser4, #ser5, #ser6, #ser7, #ser8, #ser9').removeClass('panel__item--active');
+  $(this).addClass('panel__item--active');
+});
+$('#ser4').click(function () {
+  $('#item4').addClass('services__item--active');
+  $('#item1, #item2, #item3, #item5, #item6, #item7, #item8, #item9').removeClass('services__item--active');
+  $('#ser1, #ser2, #ser3, #ser5, #ser6, #ser7, #ser8, #ser9').removeClass('panel__item--active');
+  $(this).addClass('panel__item--active');
+});
+$('#ser5').click(function () {
+  $('#item5').addClass('services__item--active');
+  $('#item1, #item2, #item3, #item4, #item6, #item7, #item8, #item9').removeClass('services__item--active');
+  $('#ser1, #ser2, #ser3, #ser4, #ser6, #ser7, #ser8, #ser9').removeClass('panel__item--active');
+  $(this).addClass('panel__item--active');
+});
+$('#ser6').click(function () {
+  $('#item6').addClass('services__item--active');
+  $('#item1, #item2, #item3, #item4, #item5, #item7, #item8, #item9').removeClass('services__item--active');
+  $('#ser1, #ser2, #ser3, #ser4, #ser5, #ser7, #ser8, #ser9').removeClass('panel__item--active');
+  $(this).addClass('panel__item--active');
+});
+$('#ser7').click(function () {
+  $('#item7').addClass('services__item--active');
+  $('#item1, #item2, #item3, #item4, #item5, #item6, #item8, #item9').removeClass('services__item--active');
+  $('#ser1, #ser2, #ser3, #ser4, #ser5, #ser6, #ser8, #ser9').removeClass('panel__item--active');
+  $(this).addClass('panel__item--active');
+});
+$('#ser8').click(function () {
+  $('#item8').addClass('services__item--active');
+  $('#item1, #item2, #item6, #item4, #item5, #item6, #item7, #item9').removeClass('services__item--active');
+  $('#ser1, #ser2, #ser3, #ser4, #ser5, #ser6, #ser7, #ser9').removeClass('panel__item--active');
+  $(this).addClass('panel__item--active');
+});
+$('#ser9').click(function () {
+  $('#item9').addClass('services__item--active');
+  $('#item1, #item2, #item3, #item4, #item5, #item6, #item7, #item8').removeClass('services__item--active');
+  $('#ser1, #ser2, #ser3, #ser4, #ser5, #ser6, #ser7, #ser8').removeClass('panel__item--active');
+  $(this).addClass('panel__item--active');
+});
