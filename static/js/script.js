70d7ce424ce96879457d9e4d393e3f07f8219ef8
Gorills <gorivanickiy@gmail.com>
2021-08-04 17:12:50 +0700

    Правки

--- static/js/script.js
+++ static/js/script.js
@@ -35,13 +35,6 @@ jQuery(document).ready(function ($) {
     }
   });
 });
-$(".header__item-drop").click(function (e) {
-  e.preventDefault();
-  $(".header__dropdown").toggleClass('header__dropdown--active');
-});
-$(".header__dropdown").mouseleave(function () {
-  $(".header__dropdown").removeClass('header__dropdown--active');
-});
 $('.spec__slider').slick({
   dots: false,
   arrows: true,
@@ -76,38 +69,6 @@ $('.slider-for').slick({
   arrows: false,
   fade: true,
   asNavFor: '.slider-nav'
-});
-$('.branch__slider').slick({
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  arrows: true,
-  asNavFor: '.branch__nav, .branch__text-nav',
-  prevArrow: "<div class='arrow-wrap prev'><i class='fas fa-long-arrow-alt-left '></i></div>",
-  nextArrow: "<div class='arrow-wrap next'><i class='fas fa-long-arrow-alt-right '></i></div>",
-  responsive: [{
-    breakpoint: 1401,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      infinite: true,
-      dots: true
-    }
-  } // You can unslick at a given breakpoint now by adding:
-  // settings: "unslick"
-  // instead of a settings object
-  ]
-});
-$('.branch__nav').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  asNavFor: '.branch__slider, .branch__text-nav',
-  infinite: true
-});
-$('.branch__text-nav').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: '.branch__slider, .branch__nav',
-  infinite: true
 });
 /* the viewport is less than 768 pixels wide */
 
@@ -151,23 +112,41 @@ $('.our-command__wrap').slick({
   // instead of a settings object
   ]
 });
-
-if (window.matchMedia("(max-width: 481px)").matches) {
-  /* the viewport is less than 768 pixels wide */
-  $('.rewiew__inner').slick({
-    infinite: true,
-    speed: 300,
-    arrows: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: true,
-    dots: false,
-    adaptiveHeight: true,
-    prevArrow: "<div class='arrow-wrap prev'><i class='fas fa-long-arrow-alt-left '></i></div>",
-    nextArrow: "<div class='arrow-wrap next'><i class='fas fa-long-arrow-alt-right '></i></div>"
-  });
-}
-
+$('.rewiew__inner').slick({
+  infinite: true,
+  speed: 300,
+  arrows: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  infinite: true,
+  dots: false,
+  adaptiveHeight: true,
+  prevArrow: "<div class='arrow-wrap prev'><i class='fas fa-long-arrow-alt-left '></i></div>",
+  nextArrow: "<div class='arrow-wrap next'><i class='fas fa-long-arrow-alt-right '></i></div>",
+  responsive: [{
+    breakpoint: 1401,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true
+    }
+  }, {
+    breakpoint: 993,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
+    }
+  }, {
+    breakpoint: 577,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  } // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+  ]
+});
 $('.about__slider').slick({
   infinite: true,
   speed: 300,
@@ -197,46 +176,9 @@ $('.equipment__nav').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   asNavFor: '.equipment__slider',
-  infinite: true
-}); // equipment__nav
-// $(document).ready(function(){
-// 	$('#output').val($('#range').val());
-// 	$('#range').mousemove(function() {
-// 		$('#output').val($('#range').val());
-// 	});
-// 	$('#output').change(function() {
-// 		$('#range').val($('#output').val());
-// 	});
-// })
-
-$(window).keyup(function (e) {
-  var target = $('label input[type=checkbox]:focus');
-
-  if (e.keyCode == 9 && $(target).length) {
-    $(target).parent().addClass('focused');
-  }
-});
-$(".range-form__clear").click(function () {
-  $('input[type=checkbox]').each(function () {
-    this.checked = false;
-  });
-  $('.range-form__clear').removeClass('range-form__clear--active');
-});
-$(".checkbox").click(function () {
-  $('.range-form__clear').addClass('range-form__clear--active');
-});
-$('.range-form__btn').click(function (e) {
-  e.preventDefault();
-  var checkboxes = [];
-  $('input:checkbox:checked').each(function () {
-    checkboxes.push(this.value);
-  });
-  $('.popup').addClass('popup--active');
-  $('#value').val(checkboxes.join(', '));
-  $('#old').val($('#range').val());
-});
-$('.popup__closer, .popup__cancel').click(function () {
-  $('.popup').removeClass('popup--active');
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
 });
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -327,11 +269,25 @@ $('#ser9').click(function () {
 $('.services__item-link').click(function (e) {
   e.preventDefault();
   $(this).children('.services__item-popup').toggleClass('services__item-popup--active');
+  $('body').toggleClass('overflow');
 });
 $('.popup__open').click(function (e) {
   e.preventDefault();
   $('.popup').addClass('popup--active');
+  $('body').addClass('overflow');
 });
 $('.popup__closer, .popup__cancel').click(function () {
   $('.popup').removeClass('popup--active');
+  $('body').removeClass('overflow');
+});
+$('.price-popup__open').click(function (e) {
+  e.preventDefault();
+  $('.price-popup').addClass('price-popup--active');
+  $(".menu-btn").removeClass('menu-btn_active');
+  $(".header").removeClass('header--active');
+  $('body').addClass('overflow');
+});
+$('.price-popup, .price-popup__closer, .price-popup__cancel').click(function () {
+  $('.price-popup').removeClass('price-popup--active');
+  $('body').removeClass('overflow');
 });
